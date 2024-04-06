9070f2910142f196db96709a0a550fe1cc0d3cb2
devalto <lancearnel143@gmail.com>
2024-04-06 02:20:14 -0700

    Co-authored-by: Jayan Pintor <CoderLearnerTime@users.noreply.github.com>

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="flex flex-row justify-between gap-4 w-screen px-4 py-2 bg-yellow-200">
+        <p className="text-lg font-semibold">Hobby Hotspot</p>
+        <div className="flex flex-row gap-4 text-lg">
+          <p className="">1</p>
+          <p className="">2</p>
+        </div>
+    </div>
+    );
+}
